e7218e422c276846ef5055e3799f5d4b4c9c0f92
Nick Ninov <n.ninov99@gmail.com>
2026-02-25 08:01:58 +0200

    create_account: docker volume
    - **TEMP solution** - Store account information from different `status-backend` runs.

--- constants.py
+++ constants.py
@@ -5,5 +5,5 @@ CREDENTIALS_PATH = os.path.join(os.path.dirname(__file__), "accounts")
 STATUS_BACKEND_PARAMS = {
     "url": "http://localhost:8080",
     "logLevel": "INFO",
-    "data_dir": os.path.join(os.path.dirname(__file__), "data-dir")
+    "data_dir": "./data-dir" # <- Used in the container
 }
