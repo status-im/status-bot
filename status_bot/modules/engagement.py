01fc0c5eda2e6b705a781e85d12b2c73fe9c6502
apentori <pentori.alexis@proton.me>
2026-08-20 19:45:08 +0200

    feat: in progress - help message
    Signed-off-by: apentori <pentori.alexis@proton.me>

--- status_bot/modules/engagement.py
+++ status_bot/modules/engagement.py
@@ -1,8 +1,9 @@
 import logging
-from prometheus_client import Counter, Gauge
+from prometheus_client import Counter
 from status_bot.constants import EventTypeEnum, NotificationCategoryEnum
 from status_bot.modules.base import BaseModule, ModuleType
 from status_bot.modules.utils import extract_contact_request
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -20,16 +21,15 @@ class EngagementBot(BaseModule):
     def on_start(self):
         logger.info("Starting module Engagement Bot")
         if self.ctx.config.settings.get("first_message") is None:
-            raise ValueError("First Message not missing from config")
-        pass
+            raise ValueError("First message not missing from config")
+        if self.ctx.config.settings.get("help_message") is None:
+            raise ValueError("Help message not missing from config")
+
     def execute(self):
         pass
 
-    def on_event(self, event_type: str, event: dict):
-        event_data = event.get("event")
-        if event_data is None or event_type != EventTypeEnum.LOCAL_NOTIFICATION.value or event_data.get("category") != NotificationCategoryEnum.CONTACT_REQUEST.value:
-            logger.debug("Not a friend request")
-            return
+
+    def hanlde_contact_request(self, event_data: dict):
         new_contact = extract_contact_request(event_data)
         self._counter.labels(type="received_request").inc()
         logger.info(f"Accepting the contact request from {new_contact.contact_name}")
@@ -40,6 +40,30 @@ class EngagementBot(BaseModule):
             chat_id=new_contact.public_key,
             message=self.ctx.config.settings.get("first_message"))
         self._counter.labels(type="first_message").inc()
+
+    def print_help_message(self, event_data: dict):
+        logger.info(json.dumps(event_data))
+        message = event_data.get("messages",[])[0]
+        if message is None:
+            raise ValueError("No message in the event")
+        dest_public_key = message.get("from")
+        from_compress_key = message.get("compressedKey")
+        logger.info(f"Message received from {dest_public_key} - {from_compress_key}")
+        if from_compress_key == "zQ3shVf6Y3DRm4Df5F5kPhA3EGNF4F4CALj45PiNYVSFdpFx4":
+            self.ctx.account.send_message(
+                chat_id=dest_public_key,
+                message=self.ctx.config.settings.get("help_message"),
+                reply_to_message_id=message.get("id"))
+
+    def on_event(self, event_type: str, event: dict):
+        event_data = event.get("event")
+        if event_data is None:
+            logger.error("Invalid event")
+            return
+        if event_type == EventTypeEnum.LOCAL_NOTIFICATION.value and event_data.get("category") == NotificationCategoryEnum.CONTACT_REQUEST.value:
+            self.hanlde_contact_request(event_data)
+        if event_type == EventTypeEnum.MESSAGE.value and event_data.get("messages") != None:
+            self.print_help_message(event_data)
 
 
     def register_metrics(self) -> None:
