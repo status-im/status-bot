ca49fde9681aa1eea32e7d03586bcf3fbdc557b1
apentori <pentori.alexis@proton.me>
2026-08-20 19:17:19 +0200

    engagement: adding metrics
    Signed-off-by: apentori <pentori.alexis@proton.me>

--- status_bot/modules/engagement.py
+++ status_bot/modules/engagement.py
@@ -1,16 +1,10 @@
 import logging
+from prometheus_client import Counter, Gauge
 from status_bot.constants import EventTypeEnum, NotificationCategoryEnum
 from status_bot.modules.base import BaseModule, ModuleType
 from status_bot.modules.utils import extract_contact_request
 
 logger = logging.getLogger(__name__)
-
-
-class NewContact():
-
-    def __init__(self, public_key:str, name: str):
-        self.public_key = public_key
-        self.name = name
 
 class EngagementBot(BaseModule):
 
@@ -33,21 +27,24 @@ class EngagementBot(BaseModule):
 
     def on_event(self, event_type: str, event: dict):
         event_data = event.get("event")
-        if event_data is None:
-            logger.info("reject")
-            return
-        logging.info(f"Received {event_type} - {EventTypeEnum.LOCAL_NOTIFICATION.value}")
-        if event_type != EventTypeEnum.LOCAL_NOTIFICATION.value:
-            logger.info("Not Correcty Type")
-            return
-        logger.info(f"Received {event_data.get('category')}")
-        if event_data.get("category") != NotificationCategoryEnum.CONTACT_REQUEST.value:
-            logger.info("rejected category")
+        if event_data is None or event_type != EventTypeEnum.LOCAL_NOTIFICATION.value or event_data.get("category") != NotificationCategoryEnum.CONTACT_REQUEST.value:
+            logger.debug("Not a friend request")
             return
         new_contact = extract_contact_request(event_data)
+        self._counter.labels(type="received_request").inc()
         logger.info(f"Accepting the contact request from {new_contact.contact_name}")
         self.ctx.account.add_contact(new_contact.public_key)
+        self._counter.labels(type="accepted_request").inc()
         logger.info(f"Sending first message to {new_contact.contact_name}")
         self.ctx.account.send_message(
             chat_id=new_contact.public_key,
             message=self.ctx.config.settings.get("first_message"))
+        self._counter.labels(type="first_message").inc()
+
+
+    def register_metrics(self) -> None:
+        self._counter = Counter(
+            "status_bot_engagement_actions",
+            "Total Contact Request received",
+            ["type"]
+        )
